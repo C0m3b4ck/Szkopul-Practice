e13ec7c7d4d95c536702d0937ea96ac15f020164
C0m3b4ck <push.decker0k@icloud.com>
2026-08-09 09:41:30 +0200

    Added excercise from CodeForces

---
+++ CodeForces/dominoes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long M, N = 0;
+    cin >> M >> N;
+    cout << (M*N) / 2 << "\n";
+}
